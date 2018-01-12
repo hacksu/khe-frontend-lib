281a87560a9e820091c05ce65f208b2cd5c9da54
Brojowski <alex.gajowski@gmail.com>
2018-01-12 00:40:32 -0500

    Listen to KHE server and allow listeners to events and messages

--- src/live.ts
+++ src/live.ts
@@ -1,11 +1,33 @@
 import * as io from "socket.io-client"
 import { API_BASE } from "./config/config"
 
+export interface ISubscriber<T> {
+    onCreate(msg: T) : void;
+    onUpdate(msg: T) : void;
+    onDelete(msg: T) : void;
+}
+
+export interface IMessageSubscriber extends ISubscriber<Message> { }
+class Message {
+    public text: string;
+}
+
+export interface IEventSubscriber extends ISubscriber<Event> { }
+class Event {
+    public title: String;
+    public description: String;
+    public start: Date;
+    public end: Date;
+    public type: String;
+    public icon: String;
+    public location: String;
+}
+
 export class LiveUpdates 
 {
     //#region Singleton Members
     private static _instance: LiveUpdates;
-    static GetInstance() : void {
+    static GetInstance() : LiveUpdates {
         if (LiveUpdates._instance === undefined)
         {
             LiveUpdates._instance = new LiveUpdates();
@@ -16,26 +38,135 @@ export class LiveUpdates
                 Notification.requestPermission(LiveUpdates._instance.initConnections)
             }
         }
+        return LiveUpdates._instance;
     }
-    //#endregion Singleton Members
+    //#endregion Singleton Members    
+
+    private messageListeners : IMessageSubscriber[];
+    private eventListeners : IEventSubscriber[];
 
     private constructor() {
-
+        this.messageListeners = [];
+        this.eventListeners = [];
     }
 
+    public SubscribeToMessages(subscriber: IMessageSubscriber) {
+        this.messageListeners.push(subscriber);
+    }
+
+    public UnSubMessages(subscriber: IMessageSubscriber) : boolean {
+        let pos = this.messageListeners.indexOf(subscriber);
+        if (pos !== -1) {
+            this.messageListeners.splice(pos, 1)
+            return true;
+        } else return false;
+    }
+
+    public SubscribeToEvents(subscriber: IEventSubscriber) {
+        this.eventListeners.push(subscriber);
+    }
+
+    public UnSubEvents(subscriber: IEventSubscriber) : boolean {
+        let pos = this.eventListeners.indexOf(subscriber);
+        if (pos !== -1) {
+            this.eventListeners.splice(pos, 1)
+            return true;
+        } else return false;
+    }
+
+    /*
+     * Since this is use in a callback,
+     * do not use 'this' as it will be undefined.
+    */
     private initConnections(permission: string) {
         if (permission !== "granted")
         {
            return;
         }
 
-        var socket: SocketIOClient.Socket = io.connect(API_BASE + '/messages');
-        socket.on('create', (data: any) => {
-            console.log(data);
-        });
+        let inst = LiveUpdates._instance;
+
+        let messages: SocketIOClient.Socket = io.connect(API_BASE + '/messages');
+        messages.on('create', inst.msgCreate);
+        messages.on('update', inst.msgUpdate);
+        messages.on('delete', inst.msgDelete);
+
+        let events: SocketIOClient.Socket = io.connect(API_BASE + '/events');
+        events.on('create', inst.eventCreate);
+        events.on('update', inst.eventUpdate);
+        events.on('delete', inst.eventDelete);
 
         new Notification("KHE Update", {
             body: "Socket set up"
         });
     }
+
+    /*
+     * Since these are use in callbacks,
+     * do not use 'this' as it will be undefined.
+    */
+    //#region Message event handlers.
+    private msgCreate(data: Message) : void {
+        let inst = LiveUpdates._instance;
+        new Notification("KHE Update", {
+            body: data.text
+        });
+        for (let i = 0; i < inst.messageListeners.length; i++)
+        {
+            inst.messageListeners[i].onCreate(data);
+        }
+    }
+
+    private msgUpdate(data: Message) : void {
+        let inst = LiveUpdates._instance;
+        console.log('Update Message:' + data);
+        for (let i = 0; i < inst.messageListeners.length; i++)
+        {
+            inst.messageListeners[i].onUpdate(data);
+        }
+    }
+
+    private msgDelete(data: Message) : void {
+        let inst = LiveUpdates._instance;
+        console.log('Delete Message:' + data);
+        for (let i = 0; i < inst.messageListeners.length; i++)
+        {
+            inst.messageListeners[i].onDelete(data);
+        }
+    }
+    //#endregion Message event handlers
+
+    /*
+     * Since these are use in callbacks,
+     * do not use 'this' as it will be undefined.
+    */
+    //#region /event event handlers
+    private eventCreate(data: Event) : void {
+        let inst = LiveUpdates._instance;
+        console.log('Create Event:' + data);
+        for (let i = 0; i < inst.eventListeners.length; i++)
+        {
+            inst.eventListeners[i].onCreate(data);
+        }
+    }
+
+    private eventUpdate(data: Event) : void {
+        let inst = LiveUpdates._instance;
+        console.log('Update Message:' + data);
+        for (let i = 0; i < inst.eventListeners.length; i++)
+        {
+            inst.eventListeners[i].onUpdate(data);
+        }
+    }
+
+    private eventDelete(data: Event) : void {
+        let inst = LiveUpdates._instance;
+        console.log('Delete Event:' + data);
+        for (let i = 0; i < inst.eventListeners.length; i++)
+        {
+            inst.eventListeners[i].onDelete(data);
+        }
+    }
+    //#endregion /event event handlers
+
 }
